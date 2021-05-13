400c98b5f4fd50c087421d856b1b8c35e5831c9a
Michael <mishavint@mail.ru>
2021-05-13 12:35:37 +0300

    Fix temp_main. Changed mainView, added some graphs

--- src/main/kotlin/visualazer/view/MainView.kt
+++ src/main/kotlin/visualazer/view/MainView.kt
@@ -3,6 +3,7 @@ package visualazer.view
 import visualazer.controller.RandomPlacementStrategy
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
+import javafx.scene.paint.Color
 import javafx.stage.FileChooser
 import javafx.stage.StageStyle
 import tornadofx.*
@@ -18,116 +19,127 @@ class MainView : View("Graph visualazer.visualazer.view.view") {
 
     override val root = borderpane {
 
-        top = hbox {
-            menubar {
-                menu("Settings") {
-                    menu("File") {
-                        item("Save to file") {
-                            action {
-                                openInternalWindow<SavingPopUp>()
+            left = borderpane {
+                top = hbox {
+                    addClass(Styles.boxBorders)
+                    menubar {
+                        menu("Settings") {
+                            menu("File") {
+                                item("Save to file") {
+                                    action {
+                                        openInternalWindow<SavingPopUp>()
+                                    }
                             }
-                        }
-                        item("Read from file") {
-                            action {
-                                fileName.value = chooseFile(
-                                    filters = arrayOf(
-                                        FileChooser.ExtensionFilter(
-                                            "Text files", "*.csv"
-                                        )
-                                    )
-                                ).checkFileName()
-                                drawNewGraph()
-                            }
-                        }
-                        separator()
-                        item("Save to SQLite") {
-                            action {
+                                item("Read from file") {
+                                    action {
+                                        fileName.value = chooseFile(
+                                            filters = arrayOf(
+                                                FileChooser.ExtensionFilter(
+                                                    "Text files", "*.csv"
+                                                )
+                                            )
+                                        ).checkFileName()
+                                    }
+                                }
+                                separator()
+                                item("Save to SQLite") {
+                                    action {
 
-                            }
-                        }
-                        item("Read from SQLite") {
-                            action {
+                                    }
+                                }
+                                item("Read from SQLite") {
+                                    action {
 
-                            }
-                        }
-                        separator()
-                        item("Save to Neo4j") {
-                            action {
+                                    }
+                                }
+                                separator()
+                                item("Save to Neo4j") {
+                                    action {
 
-                            }
-                        }
-                        item("Read from Neo4j") {
-                            action {
+                                    }
+                                }
+                                item("Read from Neo4j") {
+                                    action {
 
+                                    }
+                                }
                             }
-                        }
-                    }
-                    separator()
-                    item("Reset") {
-                        action {
-                            fileName.value = ""
-                            arrangeVertexes()
-                            modalStage?.close()
-                        }
-                    }
-                    item("Close") {
-                        action {
-                            primaryStage.close()
+                            separator()
+                            item("Reset") {
+                                action {
+                                    arrangeVertexes()
+                                    arrangeVertexes()
+                                    modalStage?.close()
+                                }
+                            }
+                            item("Close") {
+                                action {
+                                    primaryStage.close()
+                                }
+                            }
                         }
                     }
                 }
-            }
-            addClass(Styles.boxBordersForMenu)
-        }
+                center = borderpane {
+                    addClass(Styles.boxBorders)
 
-        left = borderpane {
-
-            center = borderpane {
-                addClass(Styles.boxBorders)
-
-                center = vbox {
-                    spacing = 10.0
-                    alignment = Pos.CENTER_LEFT
-                    button("Search communities") {
-                        tooltip("Leiden algorithm")
-                        useMaxWidth = true
-                        action {
-
+                    center = vbox {
+                        spacing = 10.0
+                        alignment = Pos.CENTER_LEFT
+                        button("Search communities") {
+                            tooltip("Leiden algorithm")
+                            useMaxWidth = true
+                            action {
+                                Algorithms(graph).searchCommunities()
+                            }
                         }
-                    }
-
-                    button("Search main vertices") {
-                        tooltip("Searching betweenness centrality")
-                        useMaxWidth = true
-                        action {
+                        button("Search main vertices") {
+                            tooltip("Searching betweenness centrality")
+                            useMaxWidth = true
+                            action {
                             Algorithms(graph).mainVertexes()
+                            }
                         }
                     }
-                }
-                bottom = hbox {
-                    spacing = 2.0
+                    bottom = hbox {
+                        spacing = 2.0
 
-                    button("Full Screen") {
-                        tooltip(
-                            "${if (primaryStage.isFullScreen) "close Full screen" else "enter Full screen"}\n" +
-                                    "Or just press f11 button"
-                        )
-                        action {
-                            with(primaryStage) { isFullScreen = !isFullScreen }
+                        button("Full Screen") {
+                            tooltip(
+                                "${if (primaryStage.isFullScreen) "close Full screen" else "enter Full screen"}\n" +
+                                        "Or just press f11 button"
+                            )
+                            action {
+                                with(primaryStage) { isFullScreen = !isFullScreen }
+                            }
+                            shortcut("F11")
                         }
-                        shortcut("F11")
-                    }
 
-                    button("Kittens") {
-                        action {
-                            find<Kittens>().openModal(stageStyle = StageStyle.UTILITY)
+                        button("Kittens") {
+                            action {
+                                find<Kittens>().openModal(stageStyle = StageStyle.UTILITY)
+                            }
                         }
                     }
                 }
             }
-        }
-        center {
-            add(graph)
+
+        center = borderpane {
+            top = hbox {
+                minHeight = 27.0
+                addClass(Styles.boxBordersForMenu)
+                alignment = Pos.CENTER
+                label(fileName) {
+                    font = Styles.InterMediumFont
+                }
+                style {
+                    borderColor += box(left = Color.GREY , right = Color.WHITE , bottom = Color.BLACK , top = Color.WHITE)
+                }
+            }
+            center = vbox {
+                addClass(Styles.boxBorders)
+                add(graph)
+            }
         }
     }
 
