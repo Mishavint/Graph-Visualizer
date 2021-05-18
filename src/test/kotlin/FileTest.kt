3e8d0949133a5b28a1f5e9bde20f5c2e4d06f4fe
azarn <azarnikov01@gmail.com>
2021-05-18 18:00:10 +0300

    Added FileTest.kt

---
+++ src/test/kotlin/FileTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Nested
+import visualizer.model.UndirectedGraph
+
+class FileTest {
+
+    private val graph = UndirectedGraph().apply {
+
+    }
+
+    @Nested
+    inner class Save {
+
+    }
+
+    @Nested
+    inner class Reading {
+
+    }
+}
